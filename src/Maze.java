0a098752b58a20ac5953e1bdf73b4c4396419998
jwliotine <jwlcheese@att.net>
2018-10-19 14:51:07 -0500

    Maze Skeleton

---
+++ src/Maze.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Maze {
+    private Square[][] maze;
+
+    public Maze() {
+
+    }
+
+    public boolean loadMaze(String frame)
+    {
+
+    }
+
+    public ArrayList<Square> getNeighbors(Square sq)
+    {
+
+    }
+
+    public Square getStart()
+    {
+
+    }
+    public  Square getFinish()
+    {
+
+    }
+
+    public void reset()
+    {
+
+    }
+
+    public String toString()
+    {
+
+    }
+
+}
