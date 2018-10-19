a96f4ebb1cd30bb14656208aa3cecbdb6e7ba8eb
Aadi Dhingra <adhingra@stu.naperville203.org>
2018-10-19 15:07:25 -0500

    Skeleton Maze Test

---
+++ src/MazeTest.java
@@ -0,0 +1,22 @@
+import junit.framework.TestCase;
+
+public class MazeTest extends TestCase {
+
+    public void testLoadMaze() {
+    }
+
+    public void testGetNeighbors() {
+    }
+
+    public void testGetStart() {
+    }
+
+    public void testGetFinish() {
+    }
+
+    public void testReset() {
+    }
+
+    public void testToString() {
+    }
+}
