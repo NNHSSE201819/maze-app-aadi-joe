3ad02ddd8e95966fb2384cd9d8856f7c12767fc7
jwliotine <jwlcheese@att.net>
2018-10-29 15:06:18 -0500

    made the maze solver outine

---
+++ src/MazeSolver.java
@@ -0,0 +1,32 @@
+public abstract class MazeSolver
+{
+    public abstract void makeEmpty();
+    public abstract boolean isEmpty();
+    public abstract void add(Square sq);
+    public abstract Square next();
+
+    public MazeSolver(Maze maze)
+    {
+
+    }
+
+    public boolean isSolved()
+    {
+
+    }
+
+    public String getPath()
+    {
+
+    }
+
+    public Square step()
+    {
+
+    }
+
+    public void solve()
+    {
+
+    }
+}
